66907fe69680c97efc74463b4b0cc973501bffc1
Kenny Zhang <Kenny Zhang@LAPTOP-UHMC480J.fios-router.home>
2017-11-08 20:37:38 -0500

    Added turning code, finished straight driving.

--- 20mSquare/src/org/usfirst/frc/team6880/robot/Robot.java
+++ 20mSquare/src/org/usfirst/frc/team6880/robot/Robot.java
@@ -1,9 +1,8 @@
 package org.usfirst.frc.team6880.robot;
 
-import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.SampleRobot;
-import edu.wpi.first.wpilibj.interfaces.Gyro;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.RobotDrive;
 
 /**
@@ -17,7 +16,6 @@ public class Robot extends SampleRobot {
 	RobotDrive driveSys;
 	Encoder leftEnc;
 	Encoder rightEnc;
-	Gyro gyro;
 
 	@Override
 	public void robotInit() {
@@ -26,17 +24,28 @@ public class Robot extends SampleRobot {
 		leftEnc.setDistancePerPulse(RobotMap.distPerPulse);
 		rightEnc = new Encoder(2, 3, false, Encoder.EncodingType.k4X);
 		rightEnc.setDistancePerPulse(RobotMap.distPerPulse);
-		gyro = new AnalogGyro(1);
 	}
 
 	@Override
 	public void autonomous() {
+		int numturns = 0;
 		while (true) {
-			driveSys.arcadeDrive(1.0, 0.0);
-			// If we have traveled 20, let's turn
+			// Make sure we drive straight
+			driveSys.drive(1.0, rightEnc.get() - leftEnc.get());
+			
+			// If we have traveled 20m, let's turn
 			if (leftEnc.getDistance() < 20) {
-				break;
+				// If this is the 4th time (or greater) we should turn, stop robot
+				if (numturns++ >= 4) break;
+
+				//                                                v---Distance between wheels / 2
+				while (leftEnc.getDistance() < Math.PI * Math.pow(1.0, 2.0))
+					driveSys.tankDrive(-1.0, 1.0);
+				
+				leftEnc.reset();
+				rightEnc.reset();
 			}
+			Timer.delay(.05);
 		}
 	}
 }
